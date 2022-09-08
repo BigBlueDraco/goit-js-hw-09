c617b935f6258ac14e9242647b95f8085d136d0b
BigBlueDraco <39314465+BigBlueDraco@users.noreply.github.com>
2022-09-08 10:48:29 +0000

    Deploying to gh-pages from @ BigBlueDraco/goit-js-hw-09@d36fcc84d00c988eb1053e3a4bedc492b64cbee1 🚀

--- 03-promises.627e7120.js
+++
@@ -1,2 +0,0 @@
-!function(){var o,n;(o=2,n=1500,Math.random()>.3?resolve({position:o,delay:n}):reject(error)).then((function(o){var n=o.position,c=o.delay;console.log("✅ Fulfilled promise ".concat(n," in ").concat(c,"ms"))})).catch((function(o){var n=o.position,c=o.delay;console.log("❌ Rejected promise ".concat(n," in ").concat(c,"ms"))}))}();
-//# sourceMappingURL=03-promises.627e7120.js.map
--- 03-promises.77cf8357.js
+++
@@ -1,2 +0,0 @@
-(function(e,o){if(Math.random()>.3){return resolve({position:e,delay:o})}return reject(error)})(2,1500).then((({position:e,delay:o})=>{console.log(`✅ Fulfilled promise ${e} in ${o}ms`)})).catch((({position:e,delay:o})=>{console.log(`❌ Rejected promise ${e} in ${o}ms`)}));
-//# sourceMappingURL=03-promises.77cf8357.js.map
